c65e2b4b6d14d12fcb4b013768a5f30792241bf1
NikoCheee <nikita3ch@gmail.com>
2023-07-09 02:17:14 +0300

    Add loggin and exception handling

--- openai_utils.py
+++ openai_utils.py
@@ -1,5 +1,6 @@
 from config import my_api
 import openai
+import logging
 
 openai.api_key = my_api
 
@@ -10,13 +11,17 @@ async def create_answer(text):  # TODO системне повідомлення
 
 
 async def __get_gpt_completion(text):  # TODO системне повідомлення
-    completion = await openai.ChatCompletion.acreate(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},  # TODO налаштувати
-            {"role": "user", "content": f"{text}"}
-        ]
-    )
+    try:
+        completion = await openai.ChatCompletion.acreate(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},  # TODO налаштувати
+                {"role": "user", "content": f"{text}"}
+            ]
+        )
 
-    answer = completion.choices[0].message
-    return answer['content']
+        answer = completion.choices[0].message
+        return answer['content']
+    except Exception as e:
+        logging.error(e)
+        return "На жаль, під час виконання запиту сталася помилка. Спробуйте ще раз пізніше."
